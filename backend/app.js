f5b6471345440ca1be80baf1921d012ea4f95fbd
Anil7882 <97466938+Anil7882@users.noreply.github.com>
2022-01-15 01:43:34 +0530

    email auth

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const errorMiddleware=require('./middleware/error');
+const cookieParser = require("cookie-parser"); 
+const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Route imports
+const productRoute=require("./routes/productRoute");
+
+const userRoute=require("./routes/userRoutes");
+
+app.use('/api/v1',productRoute);
+app.use('/api/v1',userRoute);
+
+// Midddleware for error
+app.use(errorMiddleware);
+
+module.exports=app;
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+} = require("../controllers/productcontollers");
+const {
+  isAuthenticatedUser,
+  authorizeRoles,
+} = require("../middleware/authentication");
+
+const router = express.Router();
+
+router
+  .route("/products")
+  .get(getAllProducts);
+router
+  .route("/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
+  .get(getProductDetails);
+
+module.exports = router;
